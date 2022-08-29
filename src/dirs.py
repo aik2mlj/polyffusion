c3f0cf48bcd38c3189dac47959609f939bcf9f9e
aik2 <aik2mlj@gmail.com>
2022-08-29 18:08:46 +0400

    add dirs.py

---
+++ src/dirs.py
@@ -0,0 +1,17 @@
+import os
+
+# dataset paths
+DATA_DIR = "data/LOP_4_bin_pnt"
+TRAIN_SPLIT_DIR = "data/train_split_pnt"
+
+# the path to store demo.
+DEMO_FOLDER = "./demo"
+
+# the path to save trained model params and tensorboard log.
+RESULT_PATH = "./result"
+
+if not os.path.exists(DEMO_FOLDER):
+    os.mkdir(DEMO_FOLDER)
+
+if not os.path.exists(RESULT_PATH):
+    os.mkdir(RESULT_PATH)
