70b3b68400fcbfb6375978038aab298043b5fdc7
aik2 <aik2mlj@gmail.com>
2022-10-10 16:35:38 +0400

    add remove_pickle

---
+++ src/remove_pickle.py
@@ -0,0 +1,16 @@
+import pickle
+import os
+import sys
+from dirs import *
+
+if __name__ == "__main__":
+    remove = sys.argv[1]
+    with open(f"{TRAIN_SPLIT_DIR}/musicalion.pickle", "rb") as f:
+        pic = pickle.load(f)
+    assert remove in pic[0] or remove in pic[1]
+    if remove in pic[0]:
+        pic[0].remove(remove)
+    else:
+        pic[1].remove(remove)
+    with open(f"{TRAIN_SPLIT_DIR}/musicalion.pickle", "wb") as f:
+        pickle.dump(pic, f)
